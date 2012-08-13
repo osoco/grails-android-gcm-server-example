973bce007b60ea06fd675487bb5198326fb0f2f7
deigote <diego@toharia.com>
2012-08-13 09:28:07 +0200

    Migrated to its own repo from the grails-android-gcm-plugin  repo

---
+++ test/unit/es/osoco/android/gcm/AndroidGcmSenderControllerTests.groovy
@@ -0,0 +1,17 @@
+package es.osoco.android.gcm
+
+import grails.test.*
+
+class AndroidGcmSenderControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/es/osoco/android/gcm/DeviceControllerTests.groovy
@@ -0,0 +1,17 @@
+package es.osoco.android.gcm
+
+import grails.test.*
+
+class DeviceControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
